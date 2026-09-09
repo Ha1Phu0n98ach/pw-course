59ada4aa92757dc80677e8cdacfd1637a75cb08a
Phuong Bach <haiphuong8203@gmail.com>
2026-09-09 13:43:33 +0700

    lesson-02

---
+++ tests/lesson-02/ex1.js
@@ -0,0 +1,3 @@
+const NUMBER = 12;
+let name = "my name";
+let isEven = false;
---
+++ tests/lesson-02/ex2.js
@@ -0,0 +1,6 @@
+const myName = "Alex";
+//myName = "Nagi";
+console.log(myName);
+
+// đoạn code trên sai ở chỗ đã gán biến myName là hằng số cho một giá trị khác. 
+// Biến được khai báo là hằng số, chỉ có thể gán cho một giá trị duy nhất
