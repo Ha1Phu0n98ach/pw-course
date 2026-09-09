64cb0ff6388653d22f96b869fa7ac960da414eaa
Phuong Bach <haiphuong8203@gmail.com>
2026-09-09 23:11:06 +0700

    homework lesson-03

---
+++ tests/lesson-03/ex1.js
@@ -0,0 +1,9 @@
+const NUMBER = -2;
+
+if (NUMBER > 0) {
+  console.log("Giá trị bạn nhập là số dương");
+} else if (NUMBER < 0) {
+  console.log("Giá trị bạn nhập là số âm");
+} else {
+  console.log("Số bạn nhập là số 0");
+}
---
+++ tests/lesson-03/ex2.js
@@ -0,0 +1,7 @@
+let chieuCao = 150;
+let soLeCuaChieuCao = chieuCao % 100;
+let canNangLyTuong = (soLeCuaChieuCao * 9) / 10;
+let mucCanToiDa = soLeCuaChieuCao;
+let mucCanToiThieu = (soLeCuaChieuCao * 8) / 10;
+
+console.log(canNangLyTuong + "kg", +mucCanToiDa + "kg", +mucCanToiThieu + "kg");
